3c6ca140f8dffeee92994dcb722879c1b8b56489
Noah Rosenbaum <noahrosenbaum762@gmail.com>
2021-08-12 09:26:59 -0500

    Racer Standings

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const getData = async () => {
     let season = document.querySelector('.season')
     let round = document.querySelector('.round')
-    let response = await axios.get('https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js')
+    let response = await axios.get('http://ergast.com/api/f1/<season>/<round>/driverStandings.json')
     return response.data
 };
+
